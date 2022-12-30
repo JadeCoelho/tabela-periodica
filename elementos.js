09ee4cb3d29865cb7d4bce318846711743b442c2
Jade <jademfcoelho@gmail.com>
2022-12-29 23:03:16 -0300

    comecei a tabela

---
+++ elementos.js
@@ -0,0 +1,362 @@
+let elementos = [
+    {
+        nome: "Hidrogênio",
+        sigla: "H",
+        el: "1"
+    },
+    {
+        nome: "Hélio",
+        sigla: "He",
+        el: "2"
+    },
+    {
+        nome: "Lítio",
+        sigla: "Li",
+        el: "3"
+    },
+    {
+        nome: "Berílio",
+        sigla: "Be",
+        el: "4"
+    },
+    {
+        nome: "Boro",
+        sigla: "B",
+        el: "5"
+    },
+    {
+        nome: "Carbono",
+        sigla: "C",
+        el: "6"
+    },
+    {
+        nome: "Nitrogênio",
+        sigla: "N",
+        el: "7"
+    },
+    {
+        nome: "Oxigênio",
+        sigla: "O",
+        el: "8"
+    },
+    {
+        nome: "Flúor",
+        sigla: "F",
+        el: "9"
+    },
+    {
+        nome: "Neônio",
+        sigla: "Ne",
+        el: "10"
+    },
+    {
+        nome: "Sódio",
+        sigla: "Na",
+        el: "11"
+    },
+    {
+        nome: "Magnésio",
+        sigla: "Mg",
+        el: "12"
+    },
+    {
+        nome: "Alumínio",
+        sigla: "Al",
+        el: "13"
+    },
+    {
+        nome: "Silício",
+        sigla: "Si",
+        el: "4"
+    },
+    {
+        nome: "Fósforo",
+        sigla: "P",
+        el: "5"
+    },
+    {
+        nome: "Enxofre",
+        sigla: "S",
+        el: "6"
+    },
+    {
+        nome: "Cloro",
+        sigla: "Cl",
+        el: "7"
+    },
+    {
+        nome: "Argônio",
+        sigla: "Ar",
+        el: "8"
+    },
+    {
+        nome: "Potássio",
+        sigla: "K",
+        el: "1"
+    },
+    {
+        nome: "Cálcio",
+        sigla: "Ca",
+        el: "2"
+    },
+    {
+        nome: "Escândio",
+        sigla: "Sc",
+        el: "3"
+    },
+    {
+        nome: "Titânio",
+        sigla: "Ti",
+        el: "4"
+    },
+    {
+        nome: "Vanádio",
+        sigla: "V",
+        el: "5"
+    },
+    {
+        nome: "Cromo",
+        sigla: "Cr",
+        el: "6"
+    },
+    {
+        nome: "Manganês",
+        sigla: "Mn",
+        el: "7"
+    },
+    {
+        nome: "Ferro",
+        sigla: "Fe",
+        el: "8"
+    },
+    {
+        nome: "Cobalto",
+        sigla: "Co",
+        el: "9"
+    },
+    {
+        nome: "Níquel",
+        sigla: "Ni",
+        el: "1"
+    },
+    {
+        nome: "Cobre",
+        sigla: "Cu",
+        el: "2"
+    },
+    {
+        nome: "Zinco",
+        sigla: "Zn",
+        el: "3"
+    },
+    {
+        nome: "Gálio",
+        sigla: "Ga",
+        el: "4"
+    },
+    {
+        nome: "Germânio",
+        sigla: "Ge",
+        el: "5"
+    },
+    {
+        nome: "Arsênio",
+        sigla: "As",
+        el: "6"
+    },
+    {
+        nome: "Selênio",
+        sigla: "Se",
+        el: "7"
+    },
+    {
+        nome: "Bromo",
+        sigla: "Br",
+        el: "8"
+    },
+    {
+        nome: "Criptônio",
+        sigla: "Kr",
+        el: "9"
+    },
+    {
+        nome: "Rubídio",
+        sigla: "Rb",
+        el: "1"
+    },
+    {
+        nome: "Estrôncio",
+        sigla: "Sr",
+        el: "2"
+    },
+    {
+        nome: "Ítrio",
+        sigla: "Y",
+        el: "3"
+    },
+    {
+        nome: "Zircônio",
+        sigla: "Zr",
+        el: "4"
+    },
+    {
+        nome: "Nióbio",
+        sigla: "Nb",
+        el: "5"
+    },
+    {
+        nome: "Molibdênio",
+        sigla: "Mo",
+        el: "6"
+    },
+    {
+        nome: "Tecnécio",
+        sigla: "Tc",
+        el: "7"
+    },
+    {
+        nome: "Rutênio",
+        sigla: "Ru",
+        el: "8"
+    },
+    {
+        nome: "Ródio",
+        sigla: "Rh",
+        el: "9"
+    },
+    {
+        nome: "Paládio",
+        sigla: "Pd",
+        el: "10"
+    },
+    {
+        nome: "Prata",
+        sigla: "Ag",
+        el: "11"
+    },
+    {
+        nome: "Cádmio",
+        sigla: "Cd",
+        el: "12"
+    },
+    {
+        nome: "Índio",
+        sigla: "In",
+        el: "13"
+    },
+    {
+        nome: "Estanho",
+        sigla: "Sn",
+        el: "4"
+    },
+    {
+        nome: "Antimônio",
+        sigla: "Sb",
+        el: "5"
+    },
+    {
+        nome: "Telúrio",
+        sigla: "Te",
+        el: "6"
+    },
+    {
+        nome: "Iodo",
+        sigla: "I",
+        el: "7"
+    },
+    {
+        nome: "Xenônio",
+        sigla: "Xe",
+        el: "8"
+    },
+    {
+        nome: "Césio",
+        sigla: "Cs",
+        el: "1"
+    },
+    {
+        nome: "Bário",
+        sigla: "Ba",
+        el: "2"
+    },
+    {
+        nome: "Escândio",
+        sigla: "Sc",
+        el: "3"
+    },
+    {
+        nome: "Titânio",
+        sigla: "Ti",
+        el: "4"
+    },
+    {
+        nome: "Vanádio",
+        sigla: "V",
+        el: "5"
+    },
+    {
+        nome: "Cromo",
+        sigla: "Cr",
+        el: "6"
+    },
+    {
+        nome: "Manganês",
+        sigla: "Mn",
+        el: "7"
+    },
+    {
+        nome: "Ferro",
+        sigla: "Fe",
+        el: "8"
+    },
+    {
+        nome: "Cobalto",
+        sigla: "Co",
+        el: "9"
+    },
+    {
+        nome: "Níquel",
+        sigla: "Ni",
+        el: "1"
+    },
+    {
+        nome: "Cobre",
+        sigla: "Cu",
+        el: "2"
+    },
+    {
+        nome: "Zinco",
+        sigla: "Zn",
+        el: "3"
+    },
+    {
+        nome: "Gálio",
+        sigla: "Ga",
+        el: "4"
+    },
+    {
+        nome: "Germânio",
+        sigla: "Ge",
+        el: "5"
+    },
+    {
+        nome: "Arsênio",
+        sigla: "As",
+        el: "6"
+    },
+    {
+        nome: "Selênio",
+        sigla: "Se",
+        el: "7"
+    },
+    {
+        nome: "Bromo",
+        sigla: "Br",
+        el: "8"
+    },
+    {
+        nome: "Criptônio",
+        sigla: "Kr",
+        el: "9"
+    }
+]
---
+++ script.js
@@ -0,0 +1,717 @@
+const  elementos = [
+    {
+        nome: "Hidrogênio",
+        sigla: "H",
+        el: "1"
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "Hélio",
+        sigla: "He",
+        el: "2"
+    },
+    {
+        nome: "Lítio",
+        sigla: "Li",
+        el: "3"
+    },
+    {
+        nome: "Berílio",
+        sigla: "Be",
+        el: "4"
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "Boro",
+        sigla: "B",
+        el: "5"
+    },
+    {
+        nome: "Carbono",
+        sigla: "C",
+        el: "6"
+    },
+    {
+        nome: "Nitrogênio",
+        sigla: "N",
+        el: "7"
+    },
+    {
+        nome: "Oxigênio",
+        sigla: "O",
+        el: "8"
+    },
+    {
+        nome: "Flúor",
+        sigla: "F",
+        el: "9"
+    },
+    {
+        nome: "Neônio",
+        sigla: "Ne",
+        el: "10"
+    },
+    {
+        nome: "Sódio",
+        sigla: "Na",
+        el: "11"
+    },
+    {
+        nome: "Magnésio",
+        sigla: "Mg",
+        el: "12"
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "",
+        sigla: "",
+        el: ""
+    },
+    {
+        nome: "Alumínio",
+        sigla: "Al",
+        el: "13"
+    },
+    {
+        nome: "Silício",
+        sigla: "Si",
+        el: "14"
+    },
+    {
+        nome: "Fósforo",
+        sigla: "P",
+        el: "15"
+    },
+    {
+        nome: "Enxofre",
+        sigla: "S",
+        el: "16"
+    },
+    {
+        nome: "Cloro",
+        sigla: "Cl",
+        el: "17"
+    },
+    {
+        nome: "Argônio",
+        sigla: "Ar",
+        el: "18"
+    },
+    {
+        nome: "Potássio",
+        sigla: "K",
+        el: "19"
+    },
+    {
+        nome: "Cálcio",
+        sigla: "Ca",
+        el: "20"
+    },
+    {
+        nome: "Escândio",
+        sigla: "Sc",
+        el: "21"
+    },
+    {
+        nome: "Titânio",
+        sigla: "Ti",
+        el: "22"
+    },
+    {
+        nome: "Vanádio",
+        sigla: "V",
+        el: "23"
+    },
+    {
+        nome: "Cromo",
+        sigla: "Cr",
+        el: "24"
+    },
+    {
+        nome: "Manganês",
+        sigla: "Mn",
+        el: "25"
+    },
+    {
+        nome: "Ferro",
+        sigla: "Fe",
+        el: "26"
+    },
+    {
+        nome: "Cobalto",
+        sigla: "Co",
+        el: "27"
+    },
+    {
+        nome: "Níquel",
+        sigla: "Ni",
+        el: "28"
+    },
+    {
+        nome: "Cobre",
+        sigla: "Cu",
+        el: "29"
+    },
+    {
+        nome: "Zinco",
+        sigla: "Zn",
+        el: "30"
+    },
+    {
+        nome: "Gálio",
+        sigla: "Ga",
+        el: "31"
+    },
+    {
+        nome: "Germânio",
+        sigla: "Ge",
+        el: "32"
+    },
+    {
+        nome: "Arsênio",
+        sigla: "As",
+        el: "33"
+    },
+    {
+        nome: "Selênio",
+        sigla: "Se",
+        el: "34"
+    },
+    {
+        nome: "Bromo",
+        sigla: "Br",
+        el: "35"
+    },
+    {
+        nome: "Criptônio",
+        sigla: "Kr",
+        el: "36"
+    },
+    {
+        nome: "Rubídio",
+        sigla: "Rb",
+        el: "37"
+    },
+    {
+        nome: "Estrôncio",
+        sigla: "Sr",
+        el: "38"
+    },
+    {
+        nome: "Ítrio",
+        sigla: "Y",
+        el: "39"
+    },
+    {
+        nome: "Zircônio",
+        sigla: "Zr",
+        el: "40"
+    },
+    {
+        nome: "Nióbio",
+        sigla: "Nb",
+        el: "41"
+    },
+    {
+        nome: "Molibdênio",
+        sigla: "Mo",
+        el: "42"
+    },
+    {
+        nome: "Tecnécio",
+        sigla: "Tc",
+        el: "43"
+    },
+    {
+        nome: "Rutênio",
+        sigla: "Ru",
+        el: "44"
+    },
+    {
+        nome: "Ródio",
+        sigla: "Rh",
+        el: "45"
+    },
+    {
+        nome: "Paládio",
+        sigla: "Pd",
+        el: "46"
+    },
+    {
+        nome: "Prata",
+        sigla: "Ag",
+        el: "47"
+    },
+    {
+        nome: "Cádmio",
+        sigla: "Cd",
+        el: "48"
+    },
+    {
+        nome: "Índio",
+        sigla: "In",
+        el: "49"
+    },
+    {
+        nome: "Estanho",
+        sigla: "Sn",
+        el: "50"
+    },
+    {
+        nome: "Antimônio",
+        sigla: "Sb",
+        el: "51"
+    },
+    {
+        nome: "Telúrio",
+        sigla: "Te",
+        el: "52"
+    },
+    {
+        nome: "Iodo",
+        sigla: "I",
+        el: "53"
+    },
+    {
+        nome: "Xenônio",
+        sigla: "Xe",
+        el: "54"
+    },
+    {
+        nome: "Césio",
+        sigla: "Cs",
+        el: "55"
+    },
+    {
+        nome: "Bário",
+        sigla: "Ba",
+        el: "56"
+    },
+    {
+        nome: "Lantânio",
+        sigla: "La",
+        el: "57"
+    },
+    {
+        nome: "Háfnio",
+        sigla: "Hf",
+        el: "72"
+    },
+    {
+        nome: "Tântalo",
+        sigla: "Ta",
+        el: "73"
+    },
+    {
+        nome: "Tungstênio",
+        sigla: "W",
+        el: "74"
+    },
+    {
+        nome: "Rênio",
+        sigla: "Re",
+        el: "75"
+    },
+    {
+        nome: "Ósmio",
+        sigla: "Os",
+        el: "76"
+    },
+    {
+        nome: "Irídio",
+        sigla: "Ir",
+        el: "77"
+    },
+    {
+        nome: "Platina",
+        sigla: "Pt",
+        el: "78"
+    },
+    {
+        nome: "Ouro",
+        sigla: "Au",
+        el: "79"
+    },
+    {
+        nome: "Mercúrio",
+        sigla: "Hg",
+        el: "80"
+    },
+    {
+        nome: "Tálio",
+        sigla: "Tl",
+        el: "81"
+    },
+    {
+        nome: "Chumbo",
+        sigla: "Pb",
+        el: "82"
+    },
+    {
+        nome: "Bismuto",
+        sigla: "Bi",
+        el: "83"
+    },
+    {
+        nome: "Polônio",
+        sigla: "Po",
+        el: "84"
+    },
+    {
+        nome: "Ástato",
+        sigla: "At",
+        el: "85"
+    },
+    {
+        nome: "Radônio",
+        sigla: "Rn",
+        el: "86"
+    },
+    {
+        nome: "Frâncio",
+        sigla: "Fr",
+        el: "87"
+    },
+    {
+        nome: "Rádio",
+        sigla: "Ra",
+        el: "88"
+    },
+    {
+        nome: "Actínio",
+        sigla: "Ac",
+        el: "89"
+    },
+    {
+        nome: "Rutherfórdio",
+        sigla: "Rf",
+        el: "104"
+    },
+    {
+        nome: "Dúbnio",
+        sigla: "Db",
+        el: "105"
+    },
+    {
+        nome: "Seabórgio",
+        sigla: "Sg",
+        el: "106"
+    },
+    {
+        nome: "Bóhrio",
+        sigla: "Bh",
+        el: "107"
+    },
+    {
+        nome: "Hássio",
+        sigla: "Hs",
+        el: "108"
+    },
+    {
+        nome: "Meitnério",
+        sigla: "Mt",
+        el: "109"
+    },
+    {
+        nome: "Darmstádtio",
+        sigla: "Ds",
+        el: "110"
+    },
+    {
+        nome: "Roentgênio",
+        sigla: "Rg",
+        el: "111"
+    },
+    {
+        nome: "Copernício",
+        sigla: "Cn",
+        el: "112"
+    },
+    {
+        nome: "Nihônio",
+        sigla: "Nh",
+        el: "113"
+    },
+    {
+        nome: "Fleróvio",
+        sigla: "Fl",
+        el: "114"
+    },
+    {
+        nome: "Moscóvio",
+        sigla: "Mc",
+        el: "115"
+    },
+    {
+        nome: "Livermório",
+        sigla: "Lv",
+        el: "116"
+    },
+    {
+        nome: "Tenesso",
+        sigla: "Ts",
+        el: "117"
+    },
+    {
+        nome: "Oganessônio",
+        sigla: "Og",
+        el: "118"
+    }
+]
+
+var tabela = document.querySelector('.tabela')
+frente = document.querySelector('.frente')
+verso = document.querySelector('.verso')
+
+
+
+function criaElemento() {
+
+  for (var i = 0; i < elementos.length; i++) {
+
+    let elE = document.createElement('p');
+    let siglaE = document.createElement('h2');
+    let nomeE = document.createElement('h3');
+
+    elE.innerText = elementos[i].el;
+    siglaE.innerText = elementos[i].sigla;
+    nomeE.innerText = elementos[i].nome;
+
+    elE.className = "eletron";
+    siglaE.className = "sigla";
+    nomeE.className = "nome";
+
+    let elementoE = document.createElement('div');
+    elementoE.className = "elemento";
+    let frente = document.createElement('div');
+    frente.className = "frente";
+
+    frente.appendChild(elE);
+    frente.appendChild(siglaE);
+    frente.appendChild(nomeE);
+    elementoE.appendChild(frente);
+
+    tabela.appendChild(elementoE);
+
+    if(elementos[i].el == '' && elementos[i].nome == '' && elementos[i].sigla == ''){
+        elementoE.style.visibility = 'hidden'
+    }
+        
+    elementoE.addEventListener('click', ()=>{
+        if(elementoE.classList.contains('invisivel')){
+            elementoE.classList.remove('invisivel');
+            elE.style.visibility = 'visible'
+            siglaE.style.visibility = 'visible'
+            nomeE.style.visibility = 'visible'
+        } else {
+            elementoE.classList.add('invisivel');
+            elE.style.visibility = 'hidden'
+            siglaE.style.visibility = 'hidden'
+            nomeE.style.visibility = 'hidden'    
+        }
+    })
+
+    
+    btnMostrar = document.querySelector('.mostrar')
+    btn = document.querySelector('.btn')
+    btnLimpar = document.querySelector('.limpar')
+    btnLimpar.addEventListener('click', ()=>{
+        elE.style.visibility = 'hidden'
+        siglaE.style.visibility = 'hidden'
+        nomeE.style.visibility = 'hidden'    
+    })
+
+    btnMostrar.addEventListener('click', ()=>{
+        elE.style.visibility = 'visible'
+        siglaE.style.visibility = 'visible'
+        nomeE.style.visibility = 'visible'    
+    })
+
+    btn.addEventListener('click', ()=>{
+        let span = btn.querySelector('span')
+        if(span.innerText == 'visibility'){
+            span.innerText = 'visibility_off';
+        } 
+    })
+
+
+  }
+
+}
+
+
+criaElemento()
+
+
